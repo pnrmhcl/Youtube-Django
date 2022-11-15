b97ebbb2f50eb19d3c8e7ba4f257ea7968de21e3
pnrmhcl <pmihcioglu@gmail.com>
2022-11-15 16:08:17 +0300

    feature: multiple channel

--- be/views.py
+++ be/views.py
@@ -5,7 +5,7 @@ from googleapiclient.discovery import build
 from YoutubeApi.responses import response_200
 
 api_key = 'AIzaSyDEtBuQVc_M71w5odpUzKC2TAnGZGoDF3A'
-channel_id = 'UCtXgBWNjyHrH2e-IbtIqDdQ'
+channel_ids = ['UCtXgBWNjyHrH2e-IbtIqDdQ']
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
@@ -13,13 +13,14 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def get_channel_stats(request):
+    all_data = []
     request = youtube.channels().list(
-        part='snippet,contentDetails,statistics', id=channel_id)
+        part='snippet,contentDetails,statistics', id=','.join(channel_ids))
     response = request.execute()
-    data = dict(Channel_name=response['items'][0]['snippet']['title'],
-                Subscribers=response['items'][0]['statistics']['subscriberCount'],
-                Views=response['items'][0]['statistics']['viewCount'],
-                Total_videos=response['items'][0]['statistics']['viewCount']
-
-                )
-    return response_200(data=data)
+    for i in range(len(response['items'])):
+        data = dict(Channel_name=response['items'][i]['snippet']['title'],
+                    Subscribers=response['items'][i]['statistics']['subscriberCount'],
+                    Views=response['items'][i]['statistics']['viewCount'],
+                    Total_videos=response['items'][i]['statistics']['viewCount'])
+        all_data.append(data)
+    return response_200(data=all_data)
