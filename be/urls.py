c2212a274eac039e5e59b2a368b5011e3f4e259d
pnrmhcl <pmihcioglu@gmail.com>
2022-11-15 16:29:35 +0300

    feature: get video id

--- be/urls.py
+++ be/urls.py
@@ -1,6 +1,8 @@
-from be.views import get_channel_stats
+from be.views import get_channel_stats, get_video_ids, get_channel_detail
 from django.urls import path
 
 urlpatterns = [
-    path('get-channel-stats', get_channel_stats)
+    path('get-channel-stats', get_channel_stats),
+    path('get-channel-detail', get_channel_detail),
+    path('get-video-ids', get_video_ids)
 ]
--- be/views.py
+++ be/views.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from googleapiclient.discovery import build
@@ -6,7 +7,7 @@ from YoutubeApi.responses import response_200
 
 api_key = 'AIzaSyDEtBuQVc_M71w5odpUzKC2TAnGZGoDF3A'
 channel_ids = ['UCtXgBWNjyHrH2e-IbtIqDdQ']
-
+playlist_id = 'PLEGyvPj66Tepi7iGf5NNVilCHnWyZabzq'
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 
@@ -24,3 +25,29 @@ def get_channel_stats(request):
                     Total_videos=response['items'][i]['statistics']['viewCount'])
         all_data.append(data)
     return response_200(data=all_data)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_channel_detail(request):
+    all_data = []
+    request = youtube.channels().list(
+        part='snippet,contentDetails,statistics', id=','.join(channel_ids))
+    response = request.execute()
+    for i in range(len(response['items'])):
+        data = dict(Channel_name=response['items'][i]['snippet']['title'],
+                    Subscribers=response['items'][i]['statistics']['subscriberCount'],
+                    Views=response['items'][i]['statistics']['viewCount'],
+                    Total_videos=response['items'][i]['statistics']['viewCount'],
+                    playlist_id=response['items'][i]['contentDetails']['relatedPlaylists']['uploads'])
+        all_data.append(data)
+    return response_200(data=all_data)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_video_ids(request):
+    request = youtube.playlistItems().list(part='contentDetails',
+                                           playlistId=playlist_id)
+    response = request.execute()
+    return response_200(data=response)
