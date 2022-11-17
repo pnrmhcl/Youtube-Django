3fe8fec3f1869607a6fbab3f09ad20d27ab63f0e
pnrmhcl <pmihcioglu@gmail.com>
2022-11-17 17:00:22 +0300

    fix: clear code

--- be/views.py
+++ be/views.py
@@ -1,13 +1,9 @@
-import pandas as pd
-import requests
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from googleapiclient.discovery import build
-
 from YoutubeApi.responses import response_200
 
 api_key = 'AIzaSyDEtBuQVc_M71w5odpUzKC2TAnGZGoDF3A'
-
 playlist_id = 'PLEGyvPj66Tepi7iGf5NNVilCHnWyZabzq'
 video_ids = ["z4Cxn1lDPXo", "NeSpx7vZifc", "khq8q5V3vzM"]
 youtube = build('youtube', 'v3', developerKey=api_key)
@@ -37,8 +33,7 @@ def get_single_channel(request):
     request = youtube.channels().list(part='snippet,contentDetails,statistics', id=channel_id)
     response = request.execute()
     data = dict(
-        channelName=response['items'][0]['snippet']['title'],
-        viewCount=response['items'][0]['statistics']['viewCount'],
+        channelName=response['items'][0]['snippet']['title'], viewCount=response['items'][0]['statistics']['viewCount'],
         subscriberCount=response['items'][0]['statistics']['subscriberCount'],
         videoCount=response['items'][0]['statistics']['videoCount'])
     return response_200(data=data)
@@ -65,10 +60,7 @@ def get_channel_detail(request):
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def get_video_ids(request):
-    request = youtube.playlistItems().list(part='contentDetails',
-                                           playlistId=playlist_id,
-                                           maxResults=50
-                                           )
+    request = youtube.playlistItems().list(part='contentDetails', playlistId=playlist_id, maxResults=50)
     response = request.execute()
     video_ids = []
     for i in range(len(response['items'])):
@@ -79,15 +71,12 @@ def get_video_ids(request):
         if next_page_token is None:
             more_pages = False
         else:
-            request = youtube.playlistItems().list(part='contentDetails',
-                                                   playlistId=playlist_id,
-                                                   maxResults=50,
+            request = youtube.playlistItems().list(part='contentDetails', playlistId=playlist_id, maxResults=50,
                                                    pageToken=next_page_token)
             response = request.execute()
             for i in range(len(response['items'])):
                 video_ids.append(response['items'][i]['contentDetails']['videoId'])
             next_page_token = response.get('nextPageToken')
-
     return response_200(data=video_ids)
 
 
@@ -96,19 +85,12 @@ def get_video_ids(request):
 def get_video_details(request):
     all_video_stats = []
     for i in range(0, len(video_ids), 50):
-        request = youtube.videos().list(
-            part='snippet,statistics',
-            id=','.join(video_ids[i:i + 50]))
-
+        request = youtube.videos().list(part='snippet,statistics', id=','.join(video_ids[i:i + 50]))
         response = request.execute()
-
         for video in response['items']:
-            video_stats = dict(Title=video['snippet']['title'],
-                               Published_date=video['snippet']['publishedAt'],
-                               Views=video['statistics']['viewCount'],
-                               Likes=video['statistics']['likeCount'],
-                               Comments=video['statistics']['commentCount']
-                               )
+            video_stats = dict(Title=video['snippet']['title'], Published_date=video['snippet']['publishedAt'],
+                               Views=video['statistics']['viewCount'], Likes=video['statistics']['likeCount'],
+                               Comments=video['statistics']['commentCount'])
             all_video_stats.append(video_stats)
     return response_200(data=all_video_stats)
 
@@ -117,21 +99,10 @@ def get_video_details(request):
 @permission_classes([AllowAny])
 def get_comments(request):
     all_data = []
-    request1 = youtube.commentThreads().list(
-        part="snippet",
-        videoId="kEgJBxPpPzw",
-        maxResults=100
-    ).execute()
+    request1 = youtube.commentThreads().list(part="snippet", videoId="kEgJBxPpPzw", maxResults=100).execute()
     while True:
-
-        data = request2 = youtube.commentThreads().list(
-            part="snippet",
-            videoId="NeSpx7vZifc",
-            maxResults=100,
-
-            textFormat="plainText"
-        ).execute()
-
+        data = request2 = youtube.commentThreads().list(part="snippet", videoId="NeSpx7vZifc", maxResults=100,
+                                                        textFormat="plainText").execute()
         for item in data["items"]:
             comment = item["snippet"]["topLevelComment"]
             comment_data = dict(
@@ -139,7 +110,6 @@ def get_comments(request):
                 author=comment["snippet"]["authorDisplayName"],
                 text=comment["snippet"]["textDisplay"])
             all_data.append(comment_data)
-
         return response_200(data=all_data)
 
 
@@ -148,7 +118,6 @@ def get_comments(request):
 def get_subscriber(request):
     request = youtube.subscriptions().list(
         part="snippet,contentDetails",
-        channelId="UCt5USYpzzMCYhkirVQGHwKQ"
-    )
+        channelId="UCt5USYpzzMCYhkirVQGHwKQ")
     response = request.execute()
     return response_200(data=response)
